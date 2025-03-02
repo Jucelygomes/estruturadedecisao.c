10b869c2445fa43971c0af66c821348f641ff4f3
Jucely Gomes <202503046255@alunos.estacio.br>
2025-03-02 02:49:32 +0000

    if erro

--- estrtuturadedecisao.c
+++ estrtuturadedecisao.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main (){
+    int numero1, numero2;
+
+    numero1 = 10;
+    numero2 = 5;
+
+    if (numero1 > numero2) {
+        printf("numero1 e maior que numero2");
+    }    
